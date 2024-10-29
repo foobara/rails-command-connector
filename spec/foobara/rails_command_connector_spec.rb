959e1941ede5ec347b5a02aeaff94b867f589342
Miles Georgi <azimux@gmail.com>
2024-10-29 15:15:34 -0700

    Successfully hit an endpoint in the test app

--- spec/foobara/rails_command_connector_spec.rb
+++ spec/foobara/rails_command_connector_spec.rb
@@ -1,8 +1,17 @@
+ENV["RAILS_ENV"] = "test"
+
 require_relative "../fixtures/rails-test-app/config/environment"
 require "rspec/rails"
 
 RSpec.describe Foobara::RailsCommandConnector do
   it "has a version number" do
     expect(Foobara::RailsCommandConnector::VERSION).to_not be_nil
+  end
+
+  context "when hitting a url", type: :request do
+    it "can hit a URL" do
+      get "/up"
+      expect(response).to be_successful
+    end
   end
 end
