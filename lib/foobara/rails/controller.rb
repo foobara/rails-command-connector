3f6f99a2bf9c7bd5d6cc3417f35710ffcf80640e
Miles Georgi <azimux@gmail.com>
2024-12-02 12:14:21 -0800

    DRY up controller.rb

--- lib/foobara/rails/controller.rb
+++ lib/foobara/rails/controller.rb
@@ -1,50 +1,43 @@
 class Foobara::RailsController < ApplicationController
+  before_action :run_and_set_foobara_response
+
   def run
-    # TODO: automatically put this on Rails.application.config or something
-    foobara_response = command_connector.run(self)
-
-    foobara_response.headers.each_pair do |key, value|
-      response.set_header(key, value)
-    end
-
-    render json: foobara_response.body, status: foobara_response.status
   end
 
   def help
-    foobara_response = command_connector.run(self)
-
-    foobara_response.headers.each_pair do |key, value|
-      response.set_header(key, value)
-    end
-
-    # rubocop:disable Rails/OutputSafety
-    render html: foobara_response.body.html_safe, status: foobara_response.status
-    # rubocop:enable Rails/OutputSafety
+    #     render html: foobara_response.body.html_safe, status: foobara_response.status
   end
 
   def list
-    foobara_response = command_connector.run(self)
-
-    foobara_response.headers.each_pair do |key, value|
-      response.set_header(key, value)
-    end
-
-    render json: foobara_response.body, status: foobara_response.status
   end
 
   def describe
-    foobara_response = command_connector.run(self)
-
-    foobara_response.headers.each_pair do |key, value|
-      response.set_header(key, value)
-    end
-
-    render json: foobara_response.body, status: foobara_response.status
   end
 
   # TODO: add list and describe and manifest if it is different from describe
 
   private
+
+  def run_and_set_foobara_response
+    foobara_response = command_connector.run(self)
+
+    foobara_response.headers.each_pair do |key, value|
+      response.set_header(key, value)
+    end
+
+    format = if response.content_type == "application/json"
+               :json
+             else
+               :html
+             end
+
+    body = foobara_response.body
+    # rubocop:disable Rails/OutputSafety
+    body = body.html_safe if format == :html
+    # rubocop:enable Rails/OutputSafety
+
+    render format => body, status: foobara_response.status
+  end
 
   def command_connector
     Rails.application.config.foobara_command_connector
